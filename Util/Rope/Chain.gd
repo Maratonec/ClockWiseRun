869c2c93b859cc2b96bf5465cc99acf4556b7881
Maratonec <adamcerny630@icloud.com>
2020-05-14 19:33:50 +0200

    Ropes on platforms part 2(not working)

--- Util/Rope/Chain.gd
+++ Util/Rope/Chain.gd
@@ -1,26 +1,42 @@
 extends Node2D
 const LOOP = preload("res://Util/Rope/rope.tscn")
 const LINK = preload("res://Util/Rope/Link.tscn")
-const PLATFORM = preload("res://Util/Rope/platform.tscn")
-
+const PLATFORM1 = preload("res://Util/Rope/platform.tscn")
+const PLATFORM2 = preload("res://Util/Rope/Platform2.tscn")
+const PLATFORM3 = preload("res://Util/Rope/Platform3.tscn")
+const TOP = preload("res://Util/Rope/top.tscn")
 export (int) var loops
 
 func _ready():
 	var Parent = $Anchor
-	var Child = addLoop(Parent, 4, LOOP)
+	var Child = addLoop(Parent, 4, LOOP, 0)
 	addLink(Parent, Child)
 	Parent = Child
 	for i in range (loops):
-		Child = addLoop(Parent, 2, LOOP)
+		Child = addLoop(Parent, 2, LOOP, 0)
 		addLink(Parent, Child)
 		Parent = Child
-	Child = addLoop(Parent, 2, PLATFORM)
+	Child = addLoop(Parent, 2, PLATFORM1, 0)
 	addLink(Parent, Child)
 	Parent = Child
-func addLoop(parent, space, type):
+	Child = addLoop(Parent, 0, PLATFORM2, 16)
+	addLink(Parent, Child)
+	Parent = Child
+	Child = addLoop(Parent, 0, PLATFORM3, 16)
+	addLink(Parent, Child)
+	Parent = Child
+	for i in range (loops):
+		Child = addLoop(Parent, -2, LOOP, 0)
+		addLink(Parent, Child)
+		Parent = Child
+	Child = addLoop(Parent, -4, TOP, 0)
+	addLink(Parent, Child)
+	Parent = Child
+func addLoop(parent, space, type, position):
 	var loop = type.instance()
 	loop.position = parent.position
 	loop.position.y += space
+	loop.position.x += position
 	add_child(loop)
 	return loop
 func addLink(parent, child):
